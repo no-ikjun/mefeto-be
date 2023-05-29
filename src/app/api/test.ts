9974b690f5c284d5233cd66edc518c369caa9a26
ikjunchoi <choiikjun1101@gmail.com>
2023-05-30 06:03:41 +0900

    test : copied the sessions code

--- src/app/api/test.ts
+++ src/app/api/test.ts
@@ -1,18 +1,43 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { sql } from "@vercel/postgres";
 
-export default async function Test(req: NextApiRequest, res: NextApiResponse) {
+export default async function Sessions(req: NextApiRequest, res: NextApiResponse) {
   switch (req.method) {
     case "GET":
-      try {
-        const { rows } = await sql`SELECT * FROM sessions order by id asc;`;
-        return res.status(200).json(rows);
-      } catch (err: any) {
-        return res.status(500).json({ err });
-      }
+      return await getSessions(req, res);
     case "POST":
-      return res.status(200).json({ message: "POST" });
+      return await addSession(req, res);
     default:
-      return res.status(405).json({ message: "Method not allowed" });
+      return res.status(400).end(`Method Not Allowed`);
   }
 }
+
+const getSessions = async (req: NextApiRequest, res: NextApiResponse) => {
+  const order = req.query.order;
+  if (!order) {
+    try {
+      const { rows } = await sql`SELECT * FROM sessions order by id asc;`;
+      return res.status(200).json(rows);
+    } catch (err: any) {
+      return res.status(500).json({ err });
+    }
+  } else {
+    const temp_order = JSON.stringify(order).slice(1, -1);
+    try {
+      const { rows } = await sql`SELECT * FROM sessions where id=${temp_order};`;
+      return res.status(200).json(rows);
+    } catch (err: any) {
+      return res.status(500).json({ err });
+    }
+  }
+};
+
+const addSession = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { count, datetime, place, participant, description } = req.body;
+    const result = await sql`INSERT INTO sessions (count, datetime, place, participant, description) VALUES (${count}, ${datetime}, ${place}, ${participant}, ${description});`;
+    return res.status(200).json({ result });
+  } catch (err: any) {
+    return res.status(500).json({ err });
+  }
+};
