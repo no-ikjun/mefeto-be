d9d2c222cc3816dc61835d2a5eee634f2a4cf873
ikjunchoi <choiikjun1101@gmail.com>
2023-05-30 00:56:51 +0900

    test

--- src/app/api/test.ts
+++ src/app/api/test.ts
@@ -1,1 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { sql } from "@vercel/postgres";
+
+export default async function Test(req: NextApiRequest, res: NextApiResponse) {
+  const { rows } = await sql`SELECT * FROM members`;
+  res.status(200).json({ rows });
+}
