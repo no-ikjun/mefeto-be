462fd144287b100a40b621670b7752caf0bd56e5
ikjunchoi <choiikjun1101@gmail.com>
2023-05-30 05:46:23 +0900

    test : for postman testing get and post division

--- src/app/api/test.ts
+++ src/app/api/test.ts
@@ -2,6 +2,13 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { sql } from "@vercel/postgres";
 
 export default async function Test(req: NextApiRequest, res: NextApiResponse) {
-  const { rows } = await sql`SELECT * FROM members`;
-  res.status(200).json({ rows });
+  switch (req.method) {
+    case "GET":
+      const { rows } = await sql`SELECT * FROM users`;
+      return rows;
+    case "POST":
+      return res.status(200).json({ message: "POST" });
+    default:
+      return res.status(405).json({ message: "Method not allowed" });
+  }
 }
