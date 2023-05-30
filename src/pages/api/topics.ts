41c343e1c052aabc464515de2f970fc22640a81b
ikjunchoi <choiikjun1101@gmail.com>
2023-05-30 09:49:18 +0900

    feat : topic table name fixed

--- src/pages/api/topics.ts
+++ src/pages/api/topics.ts
@@ -16,7 +16,7 @@ const getTopics = async (req: NextApiRequest, res: NextApiResponse) => {
   const order = req.query.order;
   if (!order) {
     try {
-      const { rows } = await sql`SELECT id,title,created,modified,url FROM topics order by id asc;`;
+      const { rows } = await sql`SELECT id,title,created,modified,url FROM topic order by id asc;`;
       return res.status(200).json(rows);
     } catch (err: any) {
       return res.status(500).json({ err });
@@ -24,7 +24,7 @@ const getTopics = async (req: NextApiRequest, res: NextApiResponse) => {
   } else {
     const temp_order = JSON.stringify(order).slice(1, -1);
     try {
-      const { rows } = await sql`SELECT id,title,created,modified,url FROM topics where id=${temp_order};`;
+      const { rows } = await sql`SELECT id,title,created,modified,url FROM topic where id=${temp_order};`;
       return res.status(200).json(rows);
     } catch (err: any) {
       return res.status(500).json({ err });
@@ -35,7 +35,7 @@ const getTopics = async (req: NextApiRequest, res: NextApiResponse) => {
 const addTopics = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const { title, detail, created, modified, url } = req.body;
-    const result = await sql`INSERT INTO sessions (title, detail, created, modified, url) VALUES (${title}, ${detail}, ${created}, ${modified}, ${url});`;
+    const result = await sql`INSERT INTO topic (title, detail, created, modified, url) VALUES (${title}, ${detail}, ${created}, ${modified}, ${url});`;
     return res.status(200).json({ result });
   } catch (err: any) {
     return res.status(500).json({ err });
